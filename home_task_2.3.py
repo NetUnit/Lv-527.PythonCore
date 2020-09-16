4ff490f97c27c85a085c0592231f906622bea679
NetUnit <andriyproniyk@gmail.com>
2020-09-16 23:50:34 +0300

    Add Changes #12

--- home_task_2.3.py
+++ home_task_2.3.py
@@ -4,10 +4,25 @@ import time
 
 #CHANGE A VALUE OF VARIABLE NOT INCLUDING THE 3RD VARIABLE - USING INPUT
 
-#3.1 using input
+#3.1 Simple replacement  
+
+var_1 = 100
+var_2 = 200
+
+print (var_1, ' - var_1')
+print (var_2, ' - var_2')
+
+var_1, var_2 = var_2, var_1
+
+print (var_1, ' - var_1')
+print (var_2, ' - var_2')
+
+
+#3.2 Change a value of variable not including the 3rd variable
+# Example2
 
 variable_1 = 25
-variable_2 = 'Programiz '
+variable_2 = 'Text1 '
 
 print ('Our variables are: ' + str (variable_1) + ' and ' + str (variable_2))
 
@@ -16,7 +31,7 @@ def change1(x):
 	return variable_1 + int(x)
 
 def change2(y):
-	y = str (input ( 'add some text: '))
+	y = str (input ( 'add some text to Text1: '))
 	return variable_2 + str(y)
 
 time.sleep(1)
@@ -24,36 +39,26 @@ time.sleep(1)
 print (change1('x'))
 print (change2('y'))
 
-#3.2 change a value of variable not including the 3rd variable (own numbers)
-# Example2
-
-def change1(x):
-	return variable_1 + int(x)
-
-def change2(y):
-	return variable_2 + str(y)
-
-time.sleep(1)
-
-print (change1('250')) #
-print (change2('+100500'))
-
+#ЗМІНА ЗНАЧЕНЬ ЗМІННИХ 
 
 # 3.3 change a value of variable using replace() method (replace leter to a letter in brackets)
 # Example3
 
-variable_1 = "draft" 
-variable_2 = 'Programiz '
-time.sleep(1)
 
-print(variable_2.replace ('r', '\'r\''))
-
-# reverse replacement
+variable_1 = 'Text1'
+variable_2 = 'Text2'
+print (variable_1)
+print (variable_2)
 
 time.sleep(1)
-print('variable_1: - ' + variable_1.replace ('draft', 'Programiz'))
-print('variable_2: - ' + variable_2.replace ('Programiz', 'draft'))
 
+variable_2 = variable_2.replace ('Text2', 'Text1')
+variable_1 = variable_1.replace ('Text1', 'Text2')
+
+print (variable_1)
+print (variable_2)
+
+#ЗАМІНА ЗНАЧЕНЬ з reverse
 
 
 
